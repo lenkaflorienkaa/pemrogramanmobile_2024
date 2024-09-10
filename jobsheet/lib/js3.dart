0d506598fc8ca8528197566dd188e764ebe06870
Lenka Melinda Florienka <lenkamelinda7@gmail.com>
2024-09-10 11:16:34 +0700

    practicum3-js3

--- jobsheet/lib/js3.dart
+++ jobsheet/lib/js3.dart
@@ -1,6 +1,10 @@
 void main() {
-  String test = "true";
-  if (test == "true") {
-    print("Kebenaran");
+  for (int index = 10; index < 27; index++) {
+    if (index == 21) {
+      break; // Hentikan loop jika index sama dengan 21
+    } else if (index > 1 || index < 7) {
+      continue; // Lewati iterasi berikutnya jika index lebih besar dari 1 atau kurang dari 7
+    }
+    print(index); // Cetak index jika tidak memenuhi kondisi di atas
   }
 }
